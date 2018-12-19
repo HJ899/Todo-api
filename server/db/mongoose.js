bf36d80e24674b1c0464d8095084357cc156d2b1
HJ899 <38347541+HJ899@users.noreply.github.com>
2018-12-20 01:31:09 +0530

    Add POST /users

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useFindAndModify: false, useCreateIndex: true});
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -71,6 +71,18 @@ app.patch('/todos/:id', (req,res) => {
     }).catch(e => res.status(400).send());
 })
 
+app.post('/users',(req,res) => {
+    var body = _.pick(req.body, ['email','password']);
+    var user = new User(body);
+    user.save()
+        .then(() => user.generateAuthToken())
+        .then((token) => {
+            console.log(token);
+            res.header('x-auth',token).send(user)
+        })
+        .catch(e => res.status(400).send(e));
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}!`);
 });
