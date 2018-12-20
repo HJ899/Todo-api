f70e98f56b3bb509ef9c78400105547c38deeeab
HJ899 <38347541+HJ899@users.noreply.github.com>
2018-12-20 15:51:59 +0530

    Add login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,6 +68,20 @@ UserSchema.statics.findByToken = function(token){
     })
 }
 
+UserSchema.statics.findByCredentials = function(email,password){
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) return Promise.reject();
+        return new Promise((resolve,reject) => {
+            bcrypt.compare(password,user.password, (err,res) => {
+                if(res) resolve(user);
+                else reject(); 
+            })
+        })
+    })
+}
+
 UserSchema.pre('save', function(next){
     var user = this;
 
@@ -75,13 +89,14 @@ UserSchema.pre('save', function(next){
         bcrypt.genSalt(12, (err,salt) => {
             bcrypt.hash(user.password,salt, (err,hash) => {
                 user.password = hash;
-                next();
+                next();  
             })
         })
     }else{
         next();
     }
 });
+
 
 var User = mongoose.model('User', UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -80,9 +80,19 @@ app.post('/users',(req,res) => {
             res.header('x-auth',token).send(user)
         })
         .catch(e => res.status(400).send(e));
+});
+
+app.post('/users/login', (req,res) => {
+    var body = _.pick(req.body, ['email','password']);
+
+    User.findByCredentials(body.email,body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user)
+        })
+    }).catch(e => {
+        res.status(400).send();
+    })
 })
-
-
 
 app.get('/users/me', authenticate ,(req,res) => {
     res.send(req.user);
