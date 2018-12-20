88787a7b7ec103fe2a70927a3d91f43a9769be43
HJ899 <38347541+HJ899@users.noreply.github.com>
2018-12-20 17:33:04 +0530

    Use json file for config

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
     password: 'userOnePass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id:userOneId.toHexString(),access:'auth'},'abc123').toString()
+        token: jwt.sign({_id:userOneId.toHexString(),access:'auth'},process.env.JWT_SECRET).toString()
     }]
 
 },{
@@ -21,7 +21,7 @@ const users = [{
     password: 'userTwoPass',
     tokens: [{
         access: 'auth',
-        token: jwt.sign({_id:userTwoId.toHexString(),access:'auth'},'abc123').toString()
+        token: jwt.sign({_id:userTwoId.toHexString(),access:'auth'},process.env.JWT_SECRET).toString()
     }]
 }]
 
